99db1c5b41f3f4ee7362fc7ebc7b0fd7682ff1b4
Arish Mukarram khan <arishmukarramkhan@gmail.com>
2026-06-30 23:42:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get('*', (req, res) => {
 
 // --- START SERVER ---
 // Binding to 0.0.0.0 is critical for Railway networking
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
